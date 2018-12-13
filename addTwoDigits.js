59488d6164f501fa79b7f2225b0c77270cff7acd
Mykiel <janagar@yahoo.com>
2018-12-13 18:52:26 +0800

    initial commit

---
+++ addTwoDigits.js
@@ -0,0 +1,5 @@
+var exports = module.exports={}
+
+exports.AddTwoDigits = function(a, b) {
+    return a + b
+}
---
+++ app.js
@@ -0,0 +1,3 @@
+var addTwoDigits = require('./addTwoDigits')
+
+console.log('Sum of 5 and 6 is ', addTwoDigits.AddTwoDigits(5, 6)) 
